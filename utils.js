ab53b4a4fb34ac49b5555cf835a33646fa085492
Nathan McCutchen <n8wm@icloud.com>
2023-09-04 17:16:43 -0700

    util documentation

--- utils.js
+++ utils.js
@@ -4,8 +4,16 @@ const path = require("node:path")
 const isDir = (item) => fs.lstatSync(item).isDirectory()
 const isJsFile = (item) => item.endsWith(".js")
 
+/**
+ * Recursively handles all files and directories in a given directory.
+ * @param {string} dirName - The name of the directory to handle.
+ * @param {function} callback - The function to call for each file found.
+ * @returns {void}
+ */
 const itemHandler = (dirName, callback) => {
+  // Get the full path to the directory to handle.
   const dirPath = path.join(__dirname, dirName)
+  // Get a list of all items in the dir that are either dirs or js files.
   const items = fs
     .readdirSync(dirPath)
     .filter(
@@ -13,6 +21,7 @@ const itemHandler = (dirName, callback) => {
         isDir(path.join(dirPath, item)) || isJsFile(path.join(dirPath, item))
     )
 
+  // For each item, if it's a dir, recursively call this function, else call the callback.
   for (const item of items) {
     const itemPath = path.join(dirPath, item)
     if (isDir(itemPath)) {
